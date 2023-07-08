09a74f3c835a8b05029a968c42d651ae04f8bcdc
Zoya Afreen <zoyafreen04@gmail.com>
2023-07-08 20:10:28 +1000

    final changes

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Zoya Afreen on 06/06/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
